5b49a733630c7255154714c4cc4bc4b3e4a3eb78
Kevin Yuji Kobori <kevinyujikobori@Karols-MacBook-Pro.local>
2023-08-21 22:09:36 -0300

    Added HttpClientSpy

---
+++ test/mocks/core/external/adapters/http/http_client_spy.dart
@@ -0,0 +1,15 @@
+import 'package:cloudwalk_test_mobile_engineer_2/cloudwalk_test_mobile_engineer_2.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:multiple_result/multiple_result.dart';
+
+class HttpClientSpy extends Mock implements IHttpClient {
+  When<Future<Result<dynamic, ExternalException>>> mockRequestCall() => when(() => request(
+      url: any(named: 'url'),
+      method: any(named: 'method'),
+      body: any(named: 'body'),
+      headers: any(named: 'headers')));
+  void mockRequest(dynamic data) =>
+      mockRequestCall().thenAnswer((_) async => data);
+  void mockRequestError(ExternalErrorType error) =>
+      mockRequestCall().thenAnswer((_) async => Error(ExternalException(error)));
+}
