9926c35f32e795c204da8e2d7f217b315f28663f
Kevin Yuji Kobori <kevinyujikobori@Karols-MacBook-Pro.local>
2023-08-22 11:15:55 -0300

    Centralized object data domain rules in value_object ApodObject class across all layers

--- lib/core/domain/value_objects/apod_object.dart
+++ lib/core/domain/value_objects/apod_object.dart
@@ -16,4 +16,34 @@ abstract class ApodObject {
     required this.title,
     required this.url,
   });
+
+  @override
+  String toString() {
+    return 'ApodObject(date: $date, explanation: $explanation, hdurl: $hdurl, mediaType: $mediaType, serviceVersion: $serviceVersion, title: $title, url: $url)';
+  }
+
+  @override
+  bool operator ==(covariant ApodObject other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.date == date &&
+      other.explanation == explanation &&
+      other.hdurl == hdurl &&
+      other.mediaType == mediaType &&
+      other.serviceVersion == serviceVersion &&
+      other.title == title &&
+      other.url == url;
+  }
+
+  @override
+  int get hashCode {
+    return date.hashCode ^
+      explanation.hashCode ^
+      hdurl.hashCode ^
+      mediaType.hashCode ^
+      serviceVersion.hashCode ^
+      title.hashCode ^
+      url.hashCode;
+  }
 }
--- lib/core/presentation/viewmodels/picture_viewmodel.dart
+++ lib/core/presentation/viewmodels/picture_viewmodel.dart
@@ -11,53 +11,8 @@ class PictureViewModel extends ApodObject {
     required super.url,
   });
 
-  PictureViewModel copyWith({
-    String? date,
-    String? explanation,
-    String? hdurl,
-    String? mediaType,
-    String? serviceVersion,
-    String? title,
-    String? url,
-  }) {
-    return PictureViewModel(
-      date: date ?? this.date,
-      explanation: explanation ?? this.explanation,
-      hdurl: hdurl ?? this.hdurl,
-      mediaType: mediaType ?? this.mediaType,
-      serviceVersion: serviceVersion ?? this.serviceVersion,
-      title: title ?? this.title,
-      url: url ?? this.url,
-    );
-  }
-
   @override
   String toString() {
     return 'PictureViewModel(date: $date, explanation: $explanation, hdurl: $hdurl, mediaType: $mediaType, serviceVersion: $serviceVersion, title: $title, url: $url)';
-  }
-
-  @override
-  bool operator ==(covariant PictureViewModel other) {
-    if (identical(this, other)) return true;
-  
-    return 
-      other.date == date &&
-      other.explanation == explanation &&
-      other.hdurl == hdurl &&
-      other.mediaType == mediaType &&
-      other.serviceVersion == serviceVersion &&
-      other.title == title &&
-      other.url == url;
-  }
-
-  @override
-  int get hashCode {
-    return date.hashCode ^
-      explanation.hashCode ^
-      hdurl.hashCode ^
-      mediaType.hashCode ^
-      serviceVersion.hashCode ^
-      title.hashCode ^
-      url.hashCode;
   }
 }
