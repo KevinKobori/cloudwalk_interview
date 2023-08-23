476e2169c72931e70a97f1ac2c4b6164cd3a97da
Kevin Yuji Kobori <kevinyujikobori@Karols-MacBook-Pro.local>
2023-08-23 17:33:43 -0300

    Introduced configuration for clients and storages layers

---
+++ lib/core/configuration/factories/adapters/http/api_url_factory.dart
@@ -0,0 +1,3 @@
+String remoteAdminApiUrlFactory({required String apiKey, required String requestPath}) =>
+    'https://api.nasa.gov/planetary/apod?api_key=$apiKey&$requestPath';
+    // Ieuiin5UvhSz44qMh9rboqVMfOkYbkNebhwEtxPF // TODO: NOW
