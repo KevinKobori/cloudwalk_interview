c3e1a57cf013eeaf68df4fcbfdf79fd306a0876b
Kevin Kobori <kevinyujikobori@gmail.com>
2023-12-28 16:29:21 -0300

    refact: remove local storage

--- nasa_apod_app/lib/core/configuration/configuration.dart
+++ nasa_apod_app/lib/core/configuration/configuration.dart
@@ -2,5 +2,3 @@ export 'app_builder.dart';
 export 'app_module.dart';
 export 'factories/adapters/clients/apod_api_url_factory.dart';
 export 'factories/adapters/clients/http_client_factory.dart';
-export 'factories/adapters/device/local_admin_key_path_factory.dart';
-export 'factories/adapters/device/local_storage_adapter_factory.dart';
--- nasa_apod_app/test/mocks/core/external/external.dart
+++ nasa_apod_app/test/mocks/core/external/external.dart
@@ -1,5 +1,3 @@
 export 'services/clients/api/apod_responses_factory.dart';
 export 'services/clients/http_client_spy.dart';
-export 'services/device/directories/device_local_storage_factory.dart';
-export 'services/device/local_storage_spy.dart';
 export 'services/objects/picture_factory.dart';
