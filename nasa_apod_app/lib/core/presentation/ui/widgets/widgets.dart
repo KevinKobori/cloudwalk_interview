f4d42fedd69622926b86050d7b63a662b4e9bfd4
Kevin Kobori <kevinyujikobori@gmail.com>
2024-02-06 13:26:53 -0300

    refactor: standardize file names to snake_case
    Updated the file naming logic by removing dots and converting all file
    names to a snake_case pattern. This change enhances consistency,
    improves readability, and adheres to the common naming convention
    across the project's files and directories.

--- nasa_apod_app/lib/core/presentation/ui/widgets/widgets.dart
+++ nasa_apod_app/lib/core/presentation/ui/widgets/widgets.dart
@@ -1,3 +1,3 @@
-export 'organisms/apod.date_picker.dialog.dart';
-export 'organisms/apod.pictures_list.tile.dart';
-export 'pages/apod.reload.page.dart';
+export 'organisms/apod_date_picker_dialog.dart';
+export 'organisms/apod_pictures_list_tile.dart';
+export 'pages/apod_reload_page.dart';
